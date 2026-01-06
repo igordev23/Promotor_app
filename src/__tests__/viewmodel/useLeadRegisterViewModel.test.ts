6124bf3be9696cb2881ae9d27565387cabf67ed4
Carlos André <149840865+carlosandresamp@users.noreply.github.com>
2026-01-06 20:56:05 -0300

    test: adiciona teste TDD para remoção de lead (RED)

--- src/__tests__/viewmodel/useLeadRegisterViewModel.test.ts
+++ src/__tests__/viewmodel/useLeadRegisterViewModel.test.ts
@@ -94,4 +94,40 @@ describe("useLeadRegisterViewModel", () => {
     expect(result.current.state.error).toBe("CPF é obrigatório");
     expect(mockRepo.create).not.toHaveBeenCalled();
   });
+
+  // TDD 1
+
+  test("TDD: deveria remover um lead", async () => {
+    const lead1: Lead = {
+      id: "1",
+      nome: "Carlos",
+      cpf: "12345678900",
+      telefone: "999999999",
+    };
+
+    const lead2: Lead = {
+      id: "2",
+      nome: "Maria",
+      cpf: "09876543211",
+      telefone: "888888888",
+    };
+
+    mockRepo.getAll.mockResolvedValue([lead1, lead2]);
+    mockRepo.delete.mockResolvedValue(undefined);
+
+    const { result } = renderHook(() => useLeadRegisterViewModel());
+
+    // carrega leads
+    await act(async () => {
+      await result.current.actions.loadLeads();
+    });
+
+    // REMOVE (função ainda NÃO EXISTE)
+    await act(async () => {
+      await result.current.actions.removeLead("1");
+    });
+
+    expect(mockRepo.delete).toHaveBeenCalledWith("1");
+    expect(result.current.state.leads).toEqual([lead2]);
+  });
 });
